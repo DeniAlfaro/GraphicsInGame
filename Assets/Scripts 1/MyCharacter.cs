05c99540ee3f70c65c23e1d235393e99ac77ea57
Deni Alfaro <denilorenaalfaro@hotmail.com>
2019-05-02 18:20:23 -0700

    act

---
+++ Assets/Scripts 1/MyCharacter.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+///////
+using UnityEngine.UI;
+//Se agrega una liberia para poder controlar UI
+
+public class MyCharacter : MonoBehaviour
+{
+    Renderer render;
+    [SerializeField]
+    public float colorPower= 1f;
+
+    //Para mostrar una variable en el inspector de Unity
+    [SerializeField]
+    Slider sld_colorPower;
+
+    private void Start()
+    {
+        render = GetComponent<Renderer>();
+               
+    }
+
+    private void Update()
+    {
+        render.material.SetFloat("_ColorPower", sld_colorPower.value);
+    }
+
+}
